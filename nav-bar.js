0e14581cbe72cf1e04c94ddd0d53a6bee8833662
escottamemoir <emiliealexscott@gmail.com>
2025-04-20 12:34:21 -0500

    Initial Commit

--- nav-bar.js
+++ nav-bar.js
@@ -1,7 +1,9 @@
+
 document.addEventListener('DOMContentLoaded', () => {
     const header = document.querySelector('header');
     const heroSection = document.querySelector('.full-width-image');
     const logoImg = document.querySelector('.logo img');
+    const NavBar = document.querySelector('.desktop-nav ul li a');
 
     // Toggle fixed header and logo image when scrolling past hero section
     window.addEventListener('scroll', () => {
@@ -11,9 +13,11 @@ document.addEventListener('DOMContentLoaded', () => {
         if (scrollPosition > heroHeight) {
             header.classList.add('fixed');
             logoImg.src = '../Assets/Murray Scott.png'; // Dark logo when fixed
+            navLinks.forEach(link => link.classList.add('dark')); // Add dark class to all links
         } else {
             header.classList.remove('fixed');
             logoImg.src = '../Assets/MS Logo Light.png'; // Light logo when transparent
+            navLinks.forEach(link => link.classList.remove('dark')); // Remove dark class from all links
         }
     });
 });
